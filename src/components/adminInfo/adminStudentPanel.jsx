846d90d5cf20a5effcfebdee6f6004b121edd7df
9ightshade <aniekemeusanga@gmail.com>
2024-10-22 15:30:05 +0100

    chore:added pagination

--- src/components/adminInfo/adminStudentPanel.jsx
+++ src/components/adminInfo/adminStudentPanel.jsx
@@ -4,18 +4,31 @@ import { useEffect, useState } from "react";
 function AdminStudentPanel() {
 
     const [students, setStudents] = useState();
-    const [step, setStep] = useState();
+    const [page, setPage] = useState(1);
     const fetchUsersURL = "https://portal.rsubs.org/api/users"
+    let studentArray = null;
+
+    const nextPage = () => {
+        console.log(studentArray);
+        setPage(page + 1)
 
 
+    }
 
-    
+    const prevPage = () => {
+        setPage(page - 1)
+    }
 
+    const pagination = () => {
+
+        const startIndex = (page - 1) * 8
+        const endIndex = startIndex + 8
+        studentArray = students?.slice(startIndex, endIndex);
+        console.log(students);
+        console.log(studentArray);
+    }
 
     const fetchStudents = async () => {
-
-
-
         try {
             const token = localStorage.getItem('token')
             const response = await axios.get(fetchUsersURL, {
@@ -23,8 +36,6 @@ function AdminStudentPanel() {
             })
 
             setStudents(response.data);
-
-
 
         } catch (error) {
             console.log(error);
@@ -35,80 +46,110 @@ function AdminStudentPanel() {
 
     useEffect(() => {
         fetchStudents()
+        
+            
     }, [])
 
+    console.log(students);
+    pagination()
 
 
     return (
-        <table className=" relative text-[#39447F]" >
+        <div>
+            <table className=" relative text-[#39447F]" >
 
 
-            <thead>
-                <tr>
-                    <td className="p-2" >
-                        Student ID
-                    </td>
-                    <td className="p-2" >
-                        Name
-                    </td>
-                    <td className="p-2" >
-                        Email
-                    </td>
-                    <td className="p-2" >
-                        Verified
-                    </td>
-                </tr>
-            </thead>
+                <thead>
+                    <tr>
+                        <td className="p-2" >
+                            Student ID
+                        </td>
+                        <td className="p-2" >
+                            Name
+                        </td>
+                        <td className="p-2" >
+                            Email
+                        </td>
+                        <td className="p-2" >
+                            Verified
+                        </td>
+                    </tr>
+                </thead>
 
 
-            <tbody className="relative" >
+                <tbody className="relative" >
 
 
 
-                {
-                    !students && <div className="flex justify-center items-center w-20 h-20 bg-gray-200 rounded-full absolute top-1/2 left-1/2 ">
-                        <div className="flex gap-2">
-                            <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce"></div>
-                            <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce delay-100"></div>
-                            <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce delay-200"></div>
+                    {
+                        !students && <div className="flex justify-center items-center w-20 h-20 bg-gray-200 rounded-full absolute top-1/2 left-1/2 ">
+                            <div className="flex gap-2">
+                                <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce"></div>
+                                <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce delay-100"></div>
+                                <div className="w-3 h-3 bg-gray-800 rounded-full animate-bounce delay-200"></div>
+                            </div>
                         </div>
-                    </div>
 
-                }
+                    }
 
-                {
-                    students?.map((student) => (
-                        <tr key={student._id} >
+                    {
+                        studentArray?.map((student) => (
+                            <tr key={student._id} >
 
-                            <td className="p-2" >
-                                {
-                                    student._id
-                                }
-                            </td>
+                                <td className="p-2" >
+                                    {
+                                        student._id
+                                    }
+                                </td>
 
-                            <td className="p-2" >
-                                {
-                                    student.name
-                                }
-                            </td>
+                                <td className="p-2" >
+                                    {
+                                        student.name
+                                    }
+                                </td>
 
-                            <td className="p-2" >
-                                {
-                                    student.email
-                                }
-                            </td>
-                            <td className="p-2" >
-                                {
-                                    student.isVerified === true ? <div className="bg-green-500 h-2 w-2 rounded-full " ></div> :
-                                        <div className="bg-red-500 w-2 h-2 rounded-full" ></div>
-                                }
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
+                                <td className="p-2" >
+                                    {
+                                        student.email
+                                    }
+                                </td>
+                                <td className="p-2" >
+                                    {
+                                        student.isVerified === true ? <div className="bg-green-500 h-2 w-2 rounded-full " ></div> :
+                                            <div className="bg-red-500 w-2 h-2 rounded-full" ></div>
+                                    }
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
 
-        </table>
+            </table>
+
+            {
+                page > 1 && <button
+                    onClick={prevPage}
+                    className="prev-btn bg-[#39447F] text-white p-3 border-none rounded "
+                >
+                    Prev
+                </button>
+            }
+            <span className=" text-[#39447F] bg-white border-gray-500 p-3" >
+             { page}
+            </span>
+            {
+                page < students?.length && <button
+                    onClick={() => {
+                        pagination()
+                        nextPage()
+                    }}
+                    className="next-btn bg-[#39447F] text-white p-3 border-none rounded "
+                >
+                    Next
+                </button>
+            }
+        </div>
+
     )
 }
 
